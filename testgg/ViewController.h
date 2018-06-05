8f8b68d378738d1e2f02b0f9f8a3619231d8407d
zhuangchaoyuan <15051987332@163.com>
2018-06-05 13:35:56 +0800

    Initial Commit

---
+++ testgg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgg
+//
+//  Created by 庄超源 on 2018/6/5.
+//  Copyright © 2018年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testgg/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testgg
+//
+//  Created by 庄超源 on 2018/6/5.
+//  Copyright © 2018年 William. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
